e2ba1fc1e1b3a5ba7ca63e2f6a7ab0d33c383d50
Aroley <47256141+Aroley@users.noreply.github.com>
2026-02-17 15:46:29 +0100

    Drawing and Floating

---
+++ server/server.js
@@ -0,0 +1,33 @@
+/**
+ * Simple Socket.IO relay server
+ * Receives drawings from tablets and broadcasts them to projectors
+ */
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files (draw.html & project.html)
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  // Receive drawing from tablet
+  socket.on("drawing", (imageData) => {
+    // Broadcast to everyone else (projector)
+    socket.broadcast.emit("newDrawing", imageData);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
